a8bd9a8197f4a64dd8b30510ca25b55e04fcceec
mdarafat2201 <maccenarafat@gmail.com>
2024-03-05 20:28:17 +0000

    banner part end

---
+++ src/Component/Banner/Banner.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import Slider from "react-slick";
+import BannerImgs from "../../assets/banner.png";
+const Baner = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    appendDots: (dots) => (
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          transform: "translateY(-50%)",
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <ul style={{ margin: "0px" }}> {dots} </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <>
+        {i <= 3 && (
+          <div
+            style={{
+              width: "30px",
+              padding: "10px 0",
+              borderRight: "3px solid #fff",
+            }}
+          >
+            {i + 1 === 4 ? `${i + 1}+` : `0${i + 1}`}
+          </div>
+        )}
+      </>
+    ),
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        <div>
+          <img src={BannerImgs} alt={BannerImgs} />
+        </div>
+        <div>
+          <img src={BannerImgs} alt={BannerImgs} />
+        </div>
+        <div>
+          <img src={BannerImgs} alt={BannerImgs} />
+        </div>
+        <div>
+          <img src={BannerImgs} alt={BannerImgs} />
+        </div>
+        <div>
+          <img src={BannerImgs} alt={BannerImgs} />
+        </div>
+        <div>
+          <img src={BannerImgs} alt={BannerImgs} />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Baner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../Component/Header/Header";
 import MenuBar from "../../Component/MenuBar/MenuBar";
+import Banner from "../../Component/Banner/Banner";
 const Home = () => {
   return (
     <div>
       <Header />
       <MenuBar />
+      <Banner />
     </div>
   );
 };
