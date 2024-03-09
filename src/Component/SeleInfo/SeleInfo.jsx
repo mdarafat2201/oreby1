68b1aaf668884a0ebdc13822f4654d1a5a1c0edb
mdarafat2201 <maccenarafat@gmail.com>
2024-03-09 16:37:46 +0000

    add a new common component

--- src/Component/SeleInfo/SeleInfo.jsx
+++ src/Component/SeleInfo/SeleInfo.jsx
@@ -19,13 +19,15 @@ const SeleInfo = () => {
                 <h3 className="text-main_font_color font-bold font-DMsans text-[39px]">
                   Phones Sele
                 </h3>
-                <p className="mt-12 text-main_font_color font-normal font-DMsans text-base opacity-75">
+                <p className="mt-7 text-main_font_color font-normal font-DMsans text-base opacity-75 mb-8">
                   Up to{" "}
                   <span className="font-bold text-4xl align-middle">40%</span>{" "}
                   Sele for all phones
                 </p>
                 <Button
-                  className={"py-4 px-14 bg-main_font_color text-main_bg_color"}
+                  className={
+                    "py-4 px-14 bg-main_font_color text-main_bg_color hover:bg-slate-700"
+                  }
                   title="Shop Now"
                 />
               </div>
