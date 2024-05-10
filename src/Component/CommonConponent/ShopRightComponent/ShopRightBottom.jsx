c02fff787ebf130a6581d18c518c0d60e5ebdf16
mdarafat2201 <maccenarafat@gmail.com>
2024-05-10 22:44:44 +0100

    loading part start

--- src/Component/CommonConponent/ShopRightComponent/ShopRightBottom.jsx
+++ src/Component/CommonConponent/ShopRightComponent/ShopRightBottom.jsx
@@ -5,23 +5,40 @@ import Flex from "../Flex.jsx";
 import axios from "axios";
 import { FaAnglesLeft, FaAnglesRight } from "react-icons/fa6";
 import { ShopRightPagecontext } from "../../ShopComponent/ShopRight/ShopRight";
-import { SetProduts } from "../../../Redux/AllSlice/ProductSlice/ProductSlice.js";
+import {
+  FetcherProduct,
+  SetProduts,
+} from "../../../Redux/AllSlice/ProductSlice/ProductSlice.js";
 import { useSelector, useDispatch } from "react-redux";
 const ShopRightBottom = () => {
   const dispatch = useDispatch();
   const [allProducts, setallProducts] = useState([]);
   const [page, setpage] = useState(1);
   const value = useContext(ShopRightPagecontext);
-  useEffect(() => {
-    const ProudtDataFetcher = async () => {
-      const products = await axios.get("https://dummyjson.com/products");
-      setallProducts(products.data.products);
 
-      // set data on redux
-      dispatch(SetProduts(products.data.products));
-    };
-    ProudtDataFetcher();
+  useEffect(() => {
+    dispatch(FetcherProduct());
   }, []);
+
+  const { data, status } = useSelector((state) => state.product);
+  console.log(status.payload);
+
+  useEffect(() => {
+    if (status.payload === "IDLE") {
+      setallProducts(data.payload);
+    }
+  }, [status.payload, data.payload]);
+
+  // useEffect(() => {
+  //   const ProudtDataFetcher = async () => {
+  //     const products = await axios.get("https://dummyjson.com/products");
+  //     setallProducts(products.data.products);
+
+  //     // set data on redux
+  //     dispatch(SetProduts(products.data.products));
+  //   };
+  //   ProudtDataFetcher();
+  // }, []);
 
   //HandlePagination function implementaion
   const HandlePagination = (pageNumber) => {
@@ -36,76 +53,81 @@ const ShopRightBottom = () => {
   return (
     <>
       <div className="mt-8">
-        <div className="flex items-center justify-between flex-wrap gap-y-8">
-          {allProducts
-            ?.slice(page * value - value, page * value)
-            .map((productItem) => (
-              <div className="w-[33%]" key={productItem.id}>
-                <Card
-                  img={productItem.thumbnail}
-                  productTitle={productItem.title}
-                  colorVariant={"Balck"}
-                  price={productItem.price - productItem.discountPercentage}
-                  badge={
-                    <Button
-                      className={"py-[7px] px-8 bg-black text-white"}
-                      title={
-                        productItem.discountPercentage
-                          ? `$${productItem.discountPercentage}`
-                          : "new"
+        {allProducts && (
+          <div>
+            <div className="flex items-center justify-between flex-wrap gap-y-8">
+              {allProducts
+                ?.slice(page * value - value, page * value)
+                .map((productItem) => (
+                  <div className="w-[33%]" key={productItem.id}>
+                    <Card
+                      img={productItem.thumbnail}
+                      productTitle={productItem.title}
+                      colorVariant={"Balck"}
+                      price={productItem.price - productItem.discountPercentage}
+                      badge={
+                        <Button
+                          className={"py-[7px] px-8 bg-black text-white"}
+                          title={
+                            productItem.discountPercentage
+                              ? `$${productItem.discountPercentage}`
+                              : "new"
+                          }
+                        />
                       }
                     />
-                  }
-                />
-              </div>
-            ))}
-        </div>
-        <div className="mt-10">
-          <Flex className={"items-center justify-between"}>
-            <Flex className="items-center gap-x-2">
-              <div onClick={() => HandlePagination(page - 1)}>
-                <p
-                  className={`w-9 h-9 text-[#000000] border rounded hover:text-main_bg_color hover:bg-[#000000] flex justify-center items-center cursor-pointer 
-                  `}
-                >
-                  <FaAnglesLeft />
-                </p>
-              </div>
-
-              {[...new Array(Math.floor(allProducts.length / value) + 1)].map(
-                (item, index) => (
-                  <div key={index}>
+                  </div>
+                ))}
+            </div>
+            <div className="mt-10">
+              <Flex className={"items-center justify-between"}>
+                <Flex className="items-center gap-x-2">
+                  <div onClick={() => HandlePagination(page - 1)}>
                     <p
-                      className={`w-9 h-9  border rounded  flex justify-center items-center cursor-pointer ${
-                        index + 1 === page && "bg-[#000000] text-main_bg_color"
-                      }`}
-                      onClick={() => HandlePagination(index + 1)}
+                      className={`w-9 h-9 text-[#000000] border rounded hover:text-main_bg_color hover:bg-[#000000] flex justify-center items-center cursor-pointer 
+                  `}
                     >
-                      {index + 1}
+                      <FaAnglesLeft />
                     </p>
                   </div>
-                )
-              )}
-              <div onClick={() => HandlePagination(page + 1)}>
-                <p
-                  className={`w-9 h-9 text-[#000000] border hover:text-main_bg_color rounded hover:bg-[#000000] flex justify-center items-center cursor-pointer 
+
+                  {[
+                    ...new Array(Math.floor(allProducts.length / value) + 1),
+                  ].map((item, index) => (
+                    <div key={index}>
+                      <p
+                        className={`w-9 h-9  border rounded  flex justify-center items-center cursor-pointer ${
+                          index + 1 === page &&
+                          "bg-[#000000] text-main_bg_color"
+                        }`}
+                        onClick={() => HandlePagination(index + 1)}
+                      >
+                        {index + 1}
+                      </p>
+                    </div>
+                  ))}
+                  <div onClick={() => HandlePagination(page + 1)}>
+                    <p
+                      className={`w-9 h-9 text-[#000000] border hover:text-main_bg_color rounded hover:bg-[#000000] flex justify-center items-center cursor-pointer 
                  `}
-                >
-                  <FaAnglesRight />
-                </p>
-              </div>
-            </Flex>
-            <div>
-              <p>{`Products from ${
-                page === 1 ? page * value - value + 1 : page * value - value
-              } to ${
-                page === Math.floor(allProducts.length / value) + 1
-                  ? allProducts.length
-                  : page * value
-              } of ${allProducts.length}`}</p>
+                    >
+                      <FaAnglesRight />
+                    </p>
+                  </div>
+                </Flex>
+                <div>
+                  <p>{`Products from ${
+                    page === 1 ? page * value - value + 1 : page * value - value
+                  } to ${
+                    page === Math.floor(allProducts.length / value) + 1
+                      ? allProducts.length
+                      : page * value
+                  } of ${allProducts.length}`}</p>
+                </div>
+              </Flex>
             </div>
-          </Flex>
-        </div>
+          </div>
+        )}
       </div>
     </>
   );
--- src/Redux/AllSlice/ProductSlice/ProductSlice.js
+++ src/Redux/AllSlice/ProductSlice/ProductSlice.js
@@ -1,7 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const ApiStatus = {
+  IDLE: "IDLE",
+  LOADING: "LOADING",
+  ERROR: "ERROR",
+};
+
 const initialState = {
   data: [],
+  status: ApiStatus.IDLE,
 };
 
 export const productSlice = createSlice({
@@ -11,8 +18,27 @@ export const productSlice = createSlice({
     SetProduts: (state, payload) => {
       state.data = payload;
     },
+    setStatus: (state, payload) => {
+      state.status = payload;
+    },
   },
 });
+
+// make a thunk function for getting data
+export const FetcherProduct = () => {
+  return async function GetProduct(dispatch, getstate) {
+    try {
+      dispatch(setStatus(ApiStatus.LOADING));
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+      dispatch(SetProduts(data.products));
+      dispatch(setStatus(ApiStatus.IDLE));
+    } catch (error) {
+      console.log(error);
+      dispatch(setStatus(ApiStatus.ERROR));
+    }
+  };
+};
 // Action creators are generated for each case reducer function
-export const { SetProduts } = productSlice.actions;
+export const { SetProduts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
